fd742429157446fe67fe7de66b0ee668a62a1f4f
aisha0926 <aisha.0926@hotmail.com>
2021-02-17 11:08:16 +0000

    Adding mvvm playground

---
+++ Playground/MVVM-Playground/app/src/main/java/com/example/mvvm_project/ui/Test.kt
@@ -0,0 +1,4 @@
+package com.example.mvvm_project.ui
+
+class Test {
+}
---
+++ Playground/MVVM-Playground/app/src/main/java/com/example/mvvm_project/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm_project.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_project.R
+
+class QuotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
